9bf622db262b32dad31c4018c4be14417f5a131d
Astronaut <belozorov.volodymyr98@gmail.com>
2023-05-24 07:43:43 +0800

    first commit

---
+++ react/src/components/tabsLeft/contents/dlml/MethodSelect.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import SmallCard from "../../../custom/SmallCard"
+import Button from '@mui/material/Button'
+import {useFlagsStore} from "../../../state"
+import BasicDialog from "./dialog/BasicDialog"
+import CustomDialog from "./dialog/CustomDialog"
+import CellposeDialog from "./dialog/CellposeDialog"
+export default function MethodSelect () {
+    const DialogCellposeFlag = useFlagsStore(store => store.DialogCellposeFlag)
+    const DialogBasicFlag = useFlagsStore(store => store.DialogBasicFlag)
+    const DialogCustomFlag = useFlagsStore(store => store.DialogCustomFlag)
+    const showBasicDialog = () => {
+        useFlagsStore.setState({ DialogBasicFlag: true })
+    }
+    const showCustomDialog = () => {
+        useFlagsStore.setState({ DialogCustomFlag: true })
+    }
+    const onCall = () => {
+        console.log("Call")
+    }
+
+    return (
+        <div className=''>
+            <SmallCard title="Method Select">
+                <Button style={{color: "rgb(15, 150, 136)"}} onClick={showBasicDialog}>Basic</Button>
+                <Button style={{color: "rgb(15, 150, 136)"}} onClick={onCall}>Advance</Button>
+                <Button style={{color: "rgb(15, 150, 136)"}} onClick={showCustomDialog}>Custom</Button>
+            </SmallCard>
+            {DialogBasicFlag && <BasicDialog />}
+            {DialogCustomFlag && <CustomDialog />}
+            {DialogCellposeFlag && <CellposeDialog />}
+        </div>
+    )
+}
